81a2a041a81d230c1cd7fbe3404c16deef342c26
noah227 <noah227@foxmail.com>
2024-04-01 16:45:15 +0800

    chore: ts里加了hx的使用示例部分

--- src/index.ts
+++ src/index.ts
@@ -2,6 +2,9 @@
  * 简单字符串处理
  */
 const _ = (message: string) => {
-    return `<strong>${message}</strong>`
+    // 使用hx自然也是可以的
+    const hx = require("hbuilderx")
+    const appName = hx.env.appName
+    return `<strong>${message} | @${appName}</strong>`
 }
 export default _
